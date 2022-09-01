9a7ad8a4498923dc6a953bcc3fbb888631698805
Jarret Coyle <jarretcoyletoo@gmail.com>
2022-08-31 17:27:00 -0700

    added setupInput and ^c exit

--- play.js
+++ play.js
@@ -3,7 +3,20 @@ const net = require("net");
 const connect = require('./client')
 
 // establishes a connection with the game server
-
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  } 
+}
 
 console.log("Connecting ...");
 connect();
+setupInput();
